020210817eba2fc852f67c250a181b957c748899
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-10 14:42:08 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <h1>Vue Hello <span>World</span></h1>
+</template>
+
+<style lang="scss">
+h1 {
+  color: red;
+
+  span {
+    background-color: blue;
+  }
+}
+</style>
