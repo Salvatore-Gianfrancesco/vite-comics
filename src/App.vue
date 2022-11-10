7761f4af0a893bda9880fe49c7ea46a02c4fb503
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-11-10 15:22:44 +0100

    added "content_here" section

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,33 @@ export default {
   <!-- Site Header -->
   <SiteHeader />
 
+  <!-- Site Main -->
+  <main>
+    <section class="content_here">
+      <div class="container h-100">
+        <div class="text d-flex align-items-center h-100 fs-4">Content goes Here</div>
+      </div>
+    </section>
+  </main>
 
 </template>
 
 <style lang="scss">
+@use "./assets/scss/variables.scss" as *;
 
+.content_here {
+  height: 120px;
+  background-color: $dark;
+  color: $light;
+
+  .text::before {
+    content: "-->";
+    padding-right: 0.5rem;
+  }
+
+  .text::after {
+    content: "<--";
+    padding-left: 0.5rem;
+  }
+}
 </style>
